d5b76ff90748b8b2da8b2db48c1f380c668ca215
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2025-11-13 16:04:44 +0530

    Homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import HomePage from './pages/HomePage'
+import { Route, Routes } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
+
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="bg-[url('./src/assets/chattingApp.png')] bg-contain bg-no-repeat bg-center bg-cover">
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/profile' element={<ProfilePage/>} />
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import assets from '../assets/assets'
+
+const HomePage = () => {
+  return (
+    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%] '>
+       
+    </div>
+  )
+}
+
+export default HomePage
