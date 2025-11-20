80c2231b14e017894b367e7ded923d6693018df0
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2025-11-20 17:17:02 +0530

    RightSidebar Design

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -3,12 +3,12 @@ import assets, { messagesDummyData } from '../assets/assets'
 import { formatMessageTime } from '../lib/utlis'
 
 const ChatContainer = ({selectedUser , setSelectedUser}) => {
-  const scrollEnd = useRef()
-  useEffect(()=>{
-     if(scrollEnd.current){
-      scrollEnd.current.scrollIntoView({behavior:"smooth"})
-     }
-  },[])
+ const scrollEnd = useRef();
+useEffect(() => {
+  if (scrollEnd.current) {
+    scrollEnd.current.scrollIntoView({ behavior: "smooth" });
+  }
+}, []);
 
   return selectedUser ?  (
     <div className='h-full overflow-scroll relative backdrop-blur-lg'>
@@ -41,14 +41,14 @@ const ChatContainer = ({selectedUser , setSelectedUser}) => {
       </div> 
       {/* ------------------ bottom area ---------*/}
       <div className='absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3 '>
-         <div>
-           <input type="text" placeholder='Send a message' />
+         <div className='flex-1 flex items-center bg-gray-100/12 px-3 rounded-full '>
+           <input type="text" placeholder='Send a message' className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400' />
            <input type="file" id='image' accept='image/png , image/jpeg' hidden />
            <label htmlFor="image">
               <img src={assets.gallery_icon} alt=""  className='w-5 mr-2 cursor-pointer ' />
            </label>
          </div>
-         <img src={assets.send_button} alt="" />
+         <img src={assets.send_button} alt="" className='w-7 cursor-pointer' />
       </div>
     </div>
   ):(
