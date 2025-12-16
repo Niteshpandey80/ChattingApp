da7108172738af25a69bfd5c10f59a804e14e38c
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2025-12-16 21:16:42 +0530

    creating data base

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
-const express = require('express')
-const app = express()
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import { connectDB } from "./lib/db.js";
 
-app.get('/' , (req,res)=>{
-    res.send("hello BRo ")
-})
-app.listen(3000 , ()=>{
-    console.log("Running");
-}) 
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/status", (req, res) => {
+  res.send("Server is live");
+});
+
+await connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port : " + PORT);
+});
