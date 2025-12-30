2a8fcb694f03740d8dee99db9068f9dd18c2f09f
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2025-12-30 19:29:20 +0530

    solving vercle problem

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import http from "http";
 import { connectDB } from "./lib/db.js";
+import { initSocket } from "./lib/socket.js";
 import userRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
 
 const app = express();
+const server = http.createServer(app);
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
@@ -15,9 +18,9 @@ app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
 await connectDB();
-
-app.get("/", (req, res) => {
-  res.json({ status: "Backend running on Vercel" });
-});
-
-export default app;   // 🔥 IMPORTANT
+initSocket(server);
+if(process.env.NODE_ENV !== "production"){
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
+}
+export default server;
