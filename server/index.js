afcd453e50a7e540727b520c21a7ac949aae31cd
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2025-12-30 19:31:38 +0530

    new chnages

--- server/index.js
+++ server/index.js
@@ -1,14 +1,11 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-import http from "http";
 import { connectDB } from "./lib/db.js";
-import { initSocket } from "./lib/socket.js";
 import userRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
 
 const app = express();
-const server = http.createServer(app);
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
@@ -18,9 +15,9 @@ app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
 await connectDB();
-initSocket(server);
-if(process.env.NODE_ENV !== "production"){
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on ${PORT}`));
-}
-export default server;
+
+app.get("/", (req, res) => {
+  res.json({ status: "Backend running on Vercel" });
+});
+
+export default app;   // 🔥 IMPORTANT
